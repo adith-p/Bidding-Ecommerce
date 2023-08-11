27dee28c21f336b74eeffa5aa46d303f79750166
adith-p <adithprakash008@gmail.com>
2023-08-11 14:47:22 +0530

    Added: bookmark model class

--- auctions/models.py
+++ auctions/models.py
@@ -7,7 +7,7 @@ class User(AbstractUser):
 
 
 class Catagory(models.Model):
-    catagory_name = models.CharField(max_length=30, blank=True)
+    catagory_name = models.CharField(max_length=30, blank=True,)
 
     def __str__(self) -> str:
         return self.catagory_name
@@ -18,21 +18,36 @@ class Bids(models.Model):
     user = models.ForeignKey(
         User, on_delete=models.CASCADE, related_name='ownerUser')
 
+    bid_time = models.DateTimeField(auto_now=True)
+
+    auction_id = models.IntegerField(blank=True, null=True)
+
     def __str__(self) -> str:
-        return f"{self.user}"
+        return f"{self.id}"
 
 
 class Auction_list(models.Model):
     title = models.CharField(max_length=30, blank=False)
     created_on = models.DateTimeField(auto_now_add=True)
     last_modified = models.DateTimeField(auto_now=True)
-    image_url = models.CharField(max_length=1000, blank=True, null=True)
+    image_url = models.CharField(
+        max_length=1000, blank=True, null=True)
     desc = models.TextField(max_length=3000, null=False)
     bid = models.ForeignKey(
         Bids, on_delete=models.CASCADE, related_name='userBids', )
 
     owner = models.ForeignKey(
-        User, on_delete=models.CASCADE, related_name='ownerAuction', )
+        User, on_delete=models.CASCADE, related_name='ownerAuction')
     is_active = models.BooleanField(default=True)
     catagory = models.ForeignKey(
-        Catagory, on_delete=models.CASCADE, related_name="cat", default=" ")
+        Catagory, on_delete=models.CASCADE, related_name="cat", default=" ", null=True)
+
+    class Meta:
+        ordering = ['-created_on']
+
+
+class Bookmarks(models.Model):
+    auction_id = models.ForeignKey(
+        Auction_list, on_delete=models.CASCADE, related_name="bookmark")
+    bookmarks = models.ManyToManyField(
+        User, blank=True, null=True, related_name="bookmark_id")
